e8ce986b24dc7539e5662cff60ad17cc3e37941d
Bassemanden123 <113024875+Bassemanden123@users.noreply.github.com>
2023-01-15 22:01:40 +0100

    Delete Password.js

--- Password.js
+++
@@ -1,9 +0,0 @@
-var password = "He lies still";
-function passcheck() {
-    if(document.getElementById('pass1').value != password) {
-        alert('Wrong Password');
-        return false;
-    }
-     if(document.getElementById('pass1').value == password) {
-    }
-}
